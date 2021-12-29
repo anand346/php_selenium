542fdbaf78ce942f4b551dcd37e7edbd40d3bc45
anand raj <rajanand9039@gmail.com>
2021-12-29 23:24:23 +0530

    image downloader script firstInit

--- example1.php
+++ example1.php
@@ -1,9 +1,7 @@
 <?php 
 require_once "phpwebdriver/WebDriver.php";
- require("phpwebdriver/LocatorStrategy.php");
-
-$webdriver = new WebDriver("localhost", "4444"); $webdriver->connect("chrome");
-$webdriver->get("http://google.com"); $element = $webdriver->findElementBy(LocatorStrategy::name, "q"); $element->sendKeys(array("selenium google code" ) ); $element->submit();
-
-$webdriver->close();
+//  require("phpwebdriver/LocatorStrategy.php");
+include "test/PHPWebdriverTest.php";
+$driver = new PHPWebDriverTest();
+$driver->testWindowHandling();
 ?>
---
+++ startServer.php
@@ -0,0 +1,3 @@
+<?php
+exec("java -Dwebdriver.chrome.driver=./chromeDriver.exe -jar selenium-server-standalone-3.141.59.jar");
+?>
