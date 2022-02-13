389e494039dd867bfc21e4725a0e57e1cf529518
anand raj <rajanand9039@gmail.com>
2022-02-13 17:45:06 +0530

    update readme

--- automate_fb/automate_fb.php
+++ automate_fb/automate_fb.php
@@ -6,11 +6,11 @@ $webdriver->connect("chrome");
 $webdriver->get("http://facebook.com"); 
 $element_email_input = $webdriver->findElementBy(LocatorStrategy::id, "email");
 if($element_email_input){
-    $element_email_input->sendKeys(array("rajanandrajanand346@gmail.com"));
+    $element_email_input->sendKeys(array("email"));
 }
 $element_pass_input = $webdriver->findElementBy(LocatorStrategy::id, "pass");
 if($element_pass_input){
-    $element_pass_input->sendKeys(array("Raj@anand"));
+    $element_pass_input->sendKeys(array("password"));
 }
 $element_email_input->submit();
 $element_search_input = $webdriver->findElementBy(LocatorStrategy::xpath, "/html/body/div[1]/div/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div/label/input");
